798c855d0454e0892d721ee373ed1ad6b51caf0e
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-31 20:18:12 -0600

    fetch products from api

--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>
-    <product />
+  <div class="d-flex flex-wrap">
+    <product v-for="product in products" :key="product.id" :product="product" />
   </div>
 </template>
 
 <script>
 import Product from "@/components/Product.vue";
+import productos from "@/api/Productos";
+import withAsync from "@/helpers/withAsync";
+
 export default {
   name: "Productos",
   data() {
     return {
-      productos: [],
-      buscar: "",
+      products: [],
     };
   },
   components: {
     Product,
   },
-  mounted() {},
+  async mounted() {
+    console.log("mounted");
+    const { error, data } = await withAsync(productos.getProducts, productos);
+    console.log(data);
+    console.log(error);
+    if (error) {
+      console.error(error);
+    } else {
+      this.products = data;
+    }
+  },
 };
 </script>
