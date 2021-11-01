a673444d17af679386dd7b3142e0fa2331b2e395
Jesus6Connex <jesus.velazquez@6connex.com>
2021-11-01 08:19:06 -0600

    fetch products and user role from api

--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="d-flex flex-wrap">
-    <product v-for="product in products" :key="product.id" :product="product" />
+  <div class="d-flex flex-wrap align-stretch">
+    <product
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+      :isAdmin="userRole"
+    />
   </div>
 </template>
 
 <script>
 import Product from "@/components/Product.vue";
 import productos from "@/api/Productos";
+import account from "@/api/Account";
 import withAsync from "@/helpers/withAsync";
 
 export default {
@@ -14,21 +20,33 @@ export default {
   data() {
     return {
       products: [],
+      userRole: null,
     };
   },
   components: {
     Product,
   },
-  async mounted() {
-    console.log("mounted");
-    const { error, data } = await withAsync(productos.getProducts, productos);
-    console.log(data);
-    console.log(error);
-    if (error) {
-      console.error(error);
-    } else {
-      this.products = data;
-    }
+  methods: {
+    async fetchProducts() {
+      const { error, data } = await withAsync(productos.getProducts, productos);
+      if (error) {
+        console.error(error);
+      } else {
+        this.products = data;
+      }
+    },
+    async fetchUserRole() {
+      const { error, data } = await withAsync(account.getUserRole, account);
+      if (error) {
+        console.error(error);
+      } else {
+        this.userRole = data.admin;
+      }
+    },
+  },
+  mounted() {
+    this.fetchProducts();
+    this.fetchUserRole();
   },
 };
 </script>
