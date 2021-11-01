7bd1de36a664d43457618bfebcaae1e9cd48cd2c
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-31 20:17:54 -0600

    update card layout

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,36 +1,36 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
+  <v-card class="ma-4 fill-height" max-width="344">
     <v-img :src="product.foto" height="200px"></v-img>
-    <v-card-title> {{ product.nombre }} </v-card-title>
-    <v-card-subtitle> {{ product.descripcion }} </v-card-subtitle>
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ms-4">4.5 (413)</div>
-      </v-row>
-
-      <div class="my-4 text-subtitle-1">$ {{ product.precio }}</div>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn color="orange lighten-2" text> Añadir al Carrito </v-btn>
-
-      <v-spacer></v-spacer>
-      <v-btn color="orange" icon v-if="isAdmin">
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-      <v-btn color="orange" icon v-if="isAdmin">
-        <v-icon>mdi-delete</v-icon>
-      </v-btn>
-    </v-card-actions>
+    <div class="d-flex flex-column fill-height">
+      <v-card-title class="flex-grow-0"> {{ product.nombre }} </v-card-title>
+      <v-card-subtitle class="flex-grow-1">
+        {{ product.descripcion }}
+      </v-card-subtitle>
+      <v-card-text class="flex-grow-0">
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+        <div class="my-4 text-subtitle-1">$ {{ product.precio }}</div>
+      </v-card-text>
+      <v-card-actions class="flex-grow-0">
+        <v-btn color="orange lighten-2" text> Añadir al Carrito </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="orange" icon v-if="isAdmin">
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn color="orange" icon v-if="isAdmin">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </div>
   </v-card>
 </template>
 
